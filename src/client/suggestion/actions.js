5f2e22f5f416f8b8f91405dc4eb7766f24ea0cd3
abarriel <abarriel@student.42.fr>
2017-07-03 21:10:27 +0200

    add some test

--- src/client/suggestion/actions.js
+++
@@ -1,3 +0,0 @@
-export const SUGGESTION_USER = 'evtx:server:users:suggestion';
-export const USER_SUGGESTED = 'userSuggested';
-export const suggestionUser = () => ({ type: SUGGESTION_USER, payload: {}, replyTo: USER_SUGGESTED });
